70fcbf88250055b729739197b3a01c2611b8609d
Salahaldin-tech <salahamjad777@gmail.com>
2026-06-07 20:10:34 +0300

    Implement load_routes and get_route

---
+++ route_manager.py
@@ -0,0 +1,27 @@
+import json
+
+
+class RouteManager:
+
+    def load_routes(self, filepath: str) -> list:
+
+        try:
+            with open(filepath, "r") as f:
+                routes_data = json.load(f)
+
+            return routes_data["routes"]
+
+        except FileNotFoundError:
+            raise FileNotFoundError(
+                f"Routes data file is not found: {filepath}"
+            )
+
+    def get_route(self,routes: list,route_id: str) -> dict:
+
+        for route in routes:
+
+            if route["route_id"].upper() == route_id.upper():
+
+                return route
+
+        return None
