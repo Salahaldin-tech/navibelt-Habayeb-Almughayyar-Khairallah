39443fbd605ba785076d39c3320d6217e3252f3e
Salahaldin-tech <salahamjad777@gmail.com>
2026-06-07 21:04:56 +0300

    Add display_routes with the correct formatting

--- route_manager.py
+++ route_manager.py
@@ -4,13 +4,13 @@ import json
 class RouteManager:
 
     def load_routes(self, filepath: str) -> list:
-
+        # to open the file
         try:
             with open(filepath, "r") as f:
                 routes_data = json.load(f)
 
             return routes_data["routes"]
-
+        # file not found error
         except FileNotFoundError:
             raise FileNotFoundError(
                 f"Routes data file is not found: {filepath}"
@@ -29,9 +29,21 @@ class RouteManager:
     def get_waypoints(self,routes: list,route_id: str) -> list:
 
         route = self.get_route(routes, route_id)
-
+        #to raise the error
         if route is None:
 
             raise ValueError("Route not found")
 
         return route["waypoints"]
+    
+
+    def display_routes(self,routes: list) -> None:
+        # to print the headers
+        print(f"{'Route ID':<10} | "f"{'Name':<30} | "f"{'Distance (m)':<12} | "f"Waypoints")
+
+        print( "-" * 85)
+        # to print the data
+        for route in routes:
+
+            print(
+                f"{route['route_id']:<10} | "f"{route['name']:<30} | "f"{route['distance_m']:<12} | "f"{len(route['waypoints'])}")
